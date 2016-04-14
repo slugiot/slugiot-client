2a8d2cd7a660b8d6a38879b2175826af675fb5d5
Allison Hume <arhume@ucsc.edu>
2016-04-14 15:42:56 -0700

    updates to table structure and harness API

--- applications/client/models/tables.py
+++ applications/client/models/tables.py
@@ -41,4 +41,16 @@ db.define_table('values',
                 Field('value', 'text'),  # Json, short please
                 )
 
+############### Procedure Harness Table ###############
+
+db.define_table('procedures',
+                Field('procedure_id', 'bigint', required=True),  # key
+                Field('user_email', 'string', required=True),
+                Field('last_sync', 'datetime', default=datetime.datetime.utcnow(), required=True),
+                Field('name', 'string'),  # Name of procedure
+                Field('data', 'text', required=True),  # Actual code for procedure - is check IS_LENGTH(65536) ok?
+                # Otherwise use string and specifiy larger length
+                Field('valid', 'boolean', required=True)
+                # If scheduler should/should not access entry for this procedure
+                )
 
