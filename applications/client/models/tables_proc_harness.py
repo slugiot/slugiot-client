fd6d1fcaa1b055eb86478adeb1dd211bf18accf0
Allison Hume <arhume@ucsc.edu>
2016-04-18 22:30:20 -0700

    fixed reserved fields

--- applications/client/models/tables_proc_harness.py
+++ applications/client/models/tables_proc_harness.py
@@ -1,34 +1,14 @@
 import datetime
 
-db.define_table('logs',
-                Field('timestamp', 'datetime', default=datetime.datetime.utcnow()),
-                Field('module'),
-                Field('level', 'integer'), #  int, 0 = most important.
-                Field('message', 'text'),
-                )
-
-db.define_table('output',
-                Field('timestamp', 'datetime', default=datetime.datetime.utcnow()),
-                Field('module'),
-                Field('name'), # Name of variable
-                Field('value', 'text'), # Json, short please
-                )
-
-db.define_table('values',
-                Field('timestamp', 'datetime', default=datetime.datetime.utcnow()),
-                Field('module'),
-                Field('name'),  # Name of variable
-                Field('value', 'text'),  # Json, short please
-                )
 
 ############### Procedure Harness Table ###############
 
 db.define_table('procedures',
                 Field('procedure_id', 'bigint', required=True),  # key
-                Field('user_email', 'string', required=True),
+                Field('user_id', 'string', required=True),
                 Field('last_sync', 'datetime', default=datetime.datetime.utcnow(), required=True),
                 Field('name', 'string'),  # Name of procedure
-                Field('data', 'text', required=True),  # Actual code for procedure - is check IS_LENGTH(65536) ok?
+                Field('procedure_data', 'text', required=True),  # Actual code for procedure - is check IS_LENGTH(65536) ok?
                 # Otherwise use string and specifiy larger length
                 Field('valid', 'boolean', required=True)
                 # If scheduler should/should not access entry for this procedure
