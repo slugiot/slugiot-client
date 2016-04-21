71a30e479882b9403be050258adf66b3b3f6329d
Allison Hume <arhume@ucsc.edu>
2016-04-21 15:13:30 -0700

    first draft of filling in client functions for procedure harness

--- applications/client/models/tables.py
+++ applications/client/models/tables.py
@@ -25,11 +25,9 @@ from datetime import datetime
 ############### Procedure Harness Table ###############
 
 db.define_table('procedure',
-                Field('proc_id', 'bigint', required=True),  # key
-                Field('user_id', 'string', required=True),
+                Field('procedure_id', 'bigint', required=True),  # key
                 Field('last_update', 'datetime', required=True),
-                Field('proc_name', 'string'),  # Name of procedure
-                Field('proc_data', 'text', required=True),  # Actual code for procedure - is check IS_LENGTH(65536) ok?
+                Field('procedure_data', 'text', required=True),  # Actual code for procedure - is check IS_LENGTH(65536) ok?
                 # Otherwise use string and specifiy larger length
                 )
 
