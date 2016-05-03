9c4d6b37c50624716a041fc56ee571090a492cf6
Allison Hume <arhume@ucsc.edu>
2016-05-03 10:02:36 -0700

    can't use singular procedure for table name due to reserved word

--- applications/client/models/tables.py
+++ applications/client/models/tables.py
@@ -24,7 +24,7 @@ from datetime import datetime
 
 ############### Procedure Harness Table ###############
 
-db.define_table('procedure',
+db.define_table('procedures',
                 Field('procedure_id', 'bigint', required=True),  # key
                 Field('last_update', 'datetime', required=True),
                 Field('name', 'string') # Name of procedure
@@ -38,12 +38,12 @@ db.define_table('settings',
                 Field('procedure_id'), # Can be Null for device-wide settings.
                 Field('setting_name'),
                 Field('setting_value'), # Encoded in json-plus.
-                Field('last_updated', 'datetime', default=datetime.datetime.utcnow(), update=datetime.datetime.utcnow())
+                Field('last_updated', 'datetime', default=datetime.utcnow(), update=datetime.utcnow())
                 )
 
 # Synched client -> server
 db.define_table('logs',
-                Field('time_stamp', 'datetime', default=datetime.datetime.utcnow()),
+                Field('time_stamp', 'datetime', default=datetime.utcnow()),
                 Field('modulename'),
                 Field('log_level', 'integer'),  # int, 0 = most important.
                 Field('log_message', 'text')
@@ -51,7 +51,7 @@ db.define_table('logs',
 
 # Synched client -> server
 db.define_table('outputs',
-                Field('time_stamp', 'datetime', default=datetime.datetime.utcnow()),
+                Field('time_stamp', 'datetime', default=datetime.utcnow()),
                 Field('modulename'),
                 Field('name'),  # Name of variable
                 Field('output_value', 'text'),  # Json, short please
@@ -61,7 +61,7 @@ db.define_table('outputs',
 # Synched client -> server
 # modulename + name is a key (only one row for combination).
 db.define_table('module_values',
-                Field('time_stamp', 'datetime', default=datetime.datetime.utcnow()),
+                Field('time_stamp', 'datetime', default=datetime.utcnow()),
                 Field('modulename'),
                 Field('name'),  # Name of variable
                 Field('module_value', 'text'),  # Json, short please
@@ -69,6 +69,6 @@ db.define_table('module_values',
 
 db.define_table('synchronization_events',
                 Field('table_name'),
-                Field('time_stamp', 'datetime', default=datetime.datetime.utcnow()),
+                Field('time_stamp', 'datetime', default=datetime.utcnow()),
                 )
 
