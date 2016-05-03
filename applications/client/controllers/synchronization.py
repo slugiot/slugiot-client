a2addbbcfc71112685a2a4d29cf59ee1a22856ff
rtalari <rtalari@ucsc.edu>
2016-05-02 20:21:26 -0700

    adding comments and function descriptors

--- applications/client/controllers/synchronization.py
+++ applications/client/controllers/synchronization.py
@@ -21,25 +21,13 @@ def index():
     response.flash = T("Hello World Sync")
     return dict(message=T('Welcome to web2py!'))
 
+"""
+This function syncs the information to the server. The data from  __get_log_data is serialized into JSON and posted on the server url = server_url + "/synchronization/receive_logs"
+and a 200 status code indicates successful posting of information and the synchronization_events time_stamps are updated or an error is sent in the response along with the time_stamp
 
-def test_log_message():
-    log_message = request.vars.log_message
-    log_level = request.vars.log_level
-    import procedureapi
-    api = procedureapi.ProcedureApi("Test")
-    api.write_log(log_message, log_level)
-    return response.json({"result" : "done"})
-
-def writing_to_db():
-    current.db.logs.insert(time_stamp=datetime.utcnow(),modulename="Test",log_level=0,log_message="Writing to database0")
-    current.db.logs.insert(time_stamp=datetime.utcnow(), modulename="Test", log_level=0,log_message="Writing to database1")
-    current.db.logs.insert(time_stamp=datetime.utcnow(), modulename="Test", log_level=0,log_message="Writing to database2")
-    return response.json({"result": "done"})
-
-def reading_from_db(table_name_db,time_to_sync):
-    for row in db(db.table_name == table_name).iterselect(db.table_name):
-        print row.log_message,row.time_stamp
-
+   :return: Dictionary containing whether the request posted is a success or not and the latest synchronization time_stamp
+   :rtype: Dictionary
+"""
 @request.restful()
 def synchronize_logs():
     synchronized_timestamp = datetime.utcnow()
@@ -69,6 +57,18 @@ def synchronize_logs():
 
     return locals()
 
+
+"""
+This function gets the values from the "logs" table. It returns as a dictionary all values greater than the latest timestamp in the synchronization_events
+table and all values lesser than the paramter current_timestamp. This allows for all the values on client but not on server to be stored in a dictionary
+ready to be sycnhed.
+
+   :param p1: current_timestamp
+   :type p1: str
+   :return: dictionary of all values in the logs table > synchronization_events.time_stamp and < current_timestamp
+   :rtype: dictionary
+"""
+
 def __get_log_data(current_timestamp):
     log_db_data = db(db.logs.time_stamp >= __get_last_synchronized("logs")
                      and db.logs.time_stamp < current_timestamp
@@ -94,12 +94,14 @@ def __get_last_synchronized(table_name):
     return timestamp[0].time_stamp
 
 """
-This function takes in a table_name (logs, outputs, etc) and a timestamp
-and inserts a record to store that it was updated.  The timestamp is NOT
-defaulted to datetime.utcnow() because you need to store the timestamp
-from when you retrieved the updates, so updates that occur during a sync
-event are not lost
+This function takes in a table_name and timestamp and inserts in into the synchronization_events table
+
+   :param p1: table_name
+   :type p1: str
+   :param p1: timestamp
+   :type p1: str
 """
+
 def __set_last_synchronized(table_name, timestamp):
     db.synchronization_events.insert(table_name=table_name,time_stamp=timestamp)
 
